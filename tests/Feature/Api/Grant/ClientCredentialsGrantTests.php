7b3ac3d5dcc3a1fac6170b84ac6c0def3e242c07
Syafiq <syafiq.rezpector@gmail.com>
2019-07-28 19:50:39 +0700

    change to setup and teardown

--- tests/Feature/Api/Grant/ClientCredentialsGrantTests.php
+++ tests/Feature/Api/Grant/ClientCredentialsGrantTests.php
@@ -15,6 +15,24 @@ use Tests\TestCase;
 
 class ClientCredentialsGrantTests extends TestCase
 {
+    /**
+     * @var \Illuminate\Database\Eloquent\Model|object|null
+     */
+    private $client;
+
+    protected function setUp() : void
+    {
+        parent::setUp();
+        $this->client = DB::table('oauth_clients')->where('name', 'ClientCredentials Grant Client')->first();
+    }
+
+    protected function tearDown() : void
+    {
+        DB::table('oauth_access_tokens')->delete();
+        parent::tearDown();
+    }
+
+
     public function test_it_access_token_route_with_no_arguments_provided__bad_request()
     {
         $response = $this->post('/oauth/token');
@@ -34,12 +52,11 @@ class ClientCredentialsGrantTests extends TestCase
 
     public function test_it_access_token_route_with_right_arguments__ok()
     {
-        $client = DB::table('oauth_clients')->where('name', 'ClientCredentials Grant Client')->first();
-        self::assertThat($client, self::logicalNot(self::isNull()));
+        self::assertThat($this->client, self::logicalNot(self::isNull()));
         $body = [
             'grant_type' => 'client_credentials',
-            'client_id' => $client->{'id'},
-            'client_secret' => $client->{'secret'},
+            'client_id' => $this->client->{'id'},
+            'client_secret' => $this->client->{'secret'},
             'scope' => '*',
         ];
 
