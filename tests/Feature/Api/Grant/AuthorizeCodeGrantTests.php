26830e58f0edbbbbac623ed79ab7191310ab1f0b
Syafiq <syafiq.rezpector@gmail.com>
2019-07-28 20:38:56 +0700

    ADd authorize code grant tests

---
+++ tests/Feature/Api/Grant/AuthorizeCodeGrantTests.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * This <laravel-passport> project created by :
+ * Name         : syafiq
+ * Date / Time  : 28 July 2019, 8:38 PM.
+ * Email        : syafiq.rezpector@gmail.com
+ * Github       : syafiqq
+ */
+
+namespace Tests\Feature\Api\Grant;
+
+
+use Tests\TestCase;
+
+class AuthorizeCodeGrantTests extends TestCase
+{
+
+}
+
+?>
