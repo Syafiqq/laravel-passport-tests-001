674e74ea5f6074644a7109d4ffd25947b30a3795
Syafiq <syafiq.rezpector@gmail.com>
2019-07-28 16:47:08 +0700

    Add client credential grant tests

---
+++ tests/Feature/Api/Grant/ClientCredentialsGrantTests.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * This <laravel-passport> project created by :
+ * Name         : syafiq
+ * Date / Time  : 28 July 2019, 4:46 PM.
+ * Email        : syafiq.rezpector@gmail.com
+ * Github       : syafiqq
+ */
+
+namespace Tests\Feature\Api;
+
+
+class ClientCredentialsGrantTests
+{
+
+}
+
+?>
