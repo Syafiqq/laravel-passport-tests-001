9bcc81a4c32ed19eab8ceaaac72e207b5fc71fa3
Syafiq <syafiq.rezpector@gmail.com>
2019-07-28 20:24:39 +0700

    add test_it_access_token_route_with_no_scope__ok

--- tests/Feature/Api/Grant/PasswordGrantTests.php
+++ tests/Feature/Api/Grant/PasswordGrantTests.php
@@ -106,6 +106,26 @@ class PasswordGrantTests extends TestCase
         var_dump($access_token);
         self::assertThat($access_token, self::isNull());
     }
+
+    public function test_it_access_token_route_with_no_scope__ok()
+    {
+        self::assertThat($this->client, self::logicalNot(self::isNull()));
+        $body = [
+            'grant_type' => 'password',
+            'client_id' => $this->client->{'id'},
+            'client_secret' => $this->client->{'secret'},
+            'username' => $this->user->{'email'},
+            'password' => $this->user->{'password'},
+        ];
+
+        $response = $this->post('/oauth/token', $body);
+        var_dump($body);
+        var_dump($response->json());
+        self::assertThat($response->status(), self::equalTo(200));
+        $access_token = DB::table('oauth_access_tokens')->first();
+        var_dump($access_token);
+        self::assertThat($access_token, self::logicalNot(self::isNull()));
+    }
 }
 
 ?>
