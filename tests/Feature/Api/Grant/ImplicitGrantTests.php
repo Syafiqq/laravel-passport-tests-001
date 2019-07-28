6f580a2b416308f5e5a7cc455479dc5e540f1234
Syafiq <syafiq.rezpector@gmail.com>
2019-07-28 20:41:06 +0700

    Add Implicit Grant Tests

---
+++ tests/Feature/Api/Grant/ImplicitGrantTests.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * This <laravel-passport> project created by :
+ * Name         : syafiq
+ * Date / Time  : 28 July 2019, 8:40 PM.
+ * Email        : syafiq.rezpector@gmail.com
+ * Github       : syafiqq
+ */
+
+namespace Tests\Feature\Api\Grant;
+
+
+use Tests\TestCase;
+
+class ImplicitGrantTests extends TestCase
+{
+
+}
+
+?>
