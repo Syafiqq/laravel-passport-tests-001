0985a8b858daae830f5fc42b2638230517de7719
Syafiqq <syafiq.rezpector@gmail.com>
2019-07-29 11:42:42 +0700

    Fix uri add Authorize code grant

--- database/seeds/OauthClientSeeder.php
+++ database/seeds/OauthClientSeeder.php
@@ -13,11 +13,14 @@ class OauthClientSeeder extends Seeder
      */
     public function run()
     {
+        $user = \App\User::first();
         DB::table('oauth_clients')
             ->where('name', 'Password Grant Client')
             ->orWhere('name','ClientCredentials Grant Client')
+            ->orWhere('name','AuthorizeCode Grant Client')
             ->delete();
         Artisan::call('passport:client', ['--client' => true, '--name' => 'ClientCredentials Grant Client']);
-        Artisan::call('passport:client', ['--password' => true, '--name' => 'Password Grant Client']);
+        Artisan::call('passport:client', ['--password' => true, '--name' => 'Password Grant Client', '--redirect_uri' => url('/')]);
+        Artisan::call('passport:client', ['--name' => 'AuthorizeCode Grant Client', '--redirect_uri' => url('/'), '--user_id' => $user->{'id'}]);
     }
 }
