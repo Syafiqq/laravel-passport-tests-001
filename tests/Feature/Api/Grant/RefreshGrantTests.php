c973e29eccde1eaea0d05d3b1f8467f04baa4360
Syafiq <syafiq.rezpector@gmail.com>
2019-07-30 02:47:48 +0700

    Add Refresh Grant Tests

---
+++ tests/Feature/Api/Grant/RefreshGrantTests.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * This <laravel-passport> project created by :
+ * Name         : syafiq
+ * Date / Time  : 30 July 2019, 2:47 AM.
+ * Email        : syafiq.rezpector@gmail.com
+ * Github       : syafiqq
+ */
+
+namespace Tests\Feature\Api\Grant;
+
+
+use Tests\TestCase;
+
+class RefreshGrantTests extends TestCase
+{
+
+}
+
+?>
