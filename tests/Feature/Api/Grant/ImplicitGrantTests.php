2a724c20060f8f79b18403db8b2ed24cf076825c
Syafiq <syafiq.rezpector@gmail.com>
2019-07-30 02:41:02 +0700

    remove unnecessary request tests

--- tests/Feature/Api/Grant/ImplicitGrantTests.php
+++ tests/Feature/Api/Grant/ImplicitGrantTests.php
@@ -217,24 +217,6 @@ class ImplicitGrantTests extends TestCase
         self::assertThat($response->status(), self::equalTo(500));
         self::assertThat($response->content(), self::equalTo('Authorization request was not present in the session.'));
     }
-
-    public function test_it_access_token_route_with_no_arguments_provided__bad_request()
-    {
-        $response = $this->post('/oauth/token');
-        self::assertThat($response->status(), self::equalTo(400));
-    }
-
-    public function test_it_access_token_route_with_wrong_arguments__unauthorized()
-    {
-        $response = $this->post('/oauth/token', [
-            'grant_type' => 'authorization_code',
-            'client_id' => 'client-id',
-            'client_secret' => 'client-secret',
-            'redirect_uri' => 'http://example.com/callback',
-            'code' => 'code',
-        ]);
-        self::assertThat($response->status(), self::equalTo(401));
-    }
 }
 
 ?>
