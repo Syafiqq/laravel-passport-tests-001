585b50323e6f133ea5c89a7d008b6a998efee7c9
Syafiqq <syafiq.rezpector@gmail.com>
2019-07-29 14:57:10 +0700

    add test_it_access_token_route_with_revoked_client__unauthorized

--- tests/Feature/Api/Grant/AuthorizeCodeGrantTests.php
+++ tests/Feature/Api/Grant/AuthorizeCodeGrantTests.php
@@ -358,6 +358,38 @@ class AuthorizeCodeGrantTests extends TestCase
         self::assertThat($response->json('hint'), self::equalTo('Check the `code` parameter'));
     }
 
+    public function test_it_access_token_route_with_revoked_client__unauthorized()
+    {
+        $request = $this->getCompleteAuthorization();
+        DB::table('oauth_clients')
+            ->where('id', $this->client->{'id'})
+            ->update([
+                'revoked' => 1
+            ]);
+        $this->setUpClient();
+        self::assertThat($this->client, self::logicalNot(self::isNull()));
+        self::assertThat($this->client->{'revoked'}, self::equalTo(1));
+        $body = [
+            'grant_type' => 'authorization_code',
+            'client_id' => $this->client->{'id'},
+            'client_secret' => $this->client->{'secret'},
+            'redirect_uri' => $this->client->{'redirect'},
+            'code' => $request['code'],
+        ];
+
+        $response = $this->post('/oauth/token', $body);
+        var_dump($body);
+        var_dump($response->json());
+        self::assertThat($response->status(), self::equalTo(401));
+        $access_token = DB::table('oauth_access_tokens')->first();
+        var_dump($access_token);
+        self::assertThat($access_token, self::isNull());
+        DB::table('oauth_clients')
+            ->where('id', $this->client->{'id'})
+            ->update([
+                'revoked' => 0
+            ]);
+    }
 }
 
 ?>
